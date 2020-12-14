24c753791ff1f068156a0a2a7dfa86c04caba937
Matt Bakken <mmbakken@gmail.com>
2020-12-14 13:23:33 -0700

    Initial commit for Runlog using Create React App

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../styles/App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <header>
+        <h1>Runlog</h1>
+        <p>Better Fitbit activity UI</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
