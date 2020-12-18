263dfb7f1aef9719679f406d0ae3f4f4c4fbb092
Matt Bakken <mmbakken@gmail.com>
2020-12-17 21:42:33 -0700

    Added dev and prod environments to scripts

--- src/api.js
+++ src/api.js
@@ -1,7 +1,15 @@
 import axios from 'axios'
 
+console.log(process.env.REACT_APP_ENV)
+
+let url = 'https://www.runlog.dev/api'
+
+if (process.env.REACT_APP_ENV === 'dev') {
+  url = 'http://localhost:4000/api'
+}
+
 const APIv1 = axios.create({
-  baseURL: 'http://localhost:4000/api/v1',
+  baseURL: `${url}/v1`,
   timeout: 10000,
   withCredentials: false,
 })
