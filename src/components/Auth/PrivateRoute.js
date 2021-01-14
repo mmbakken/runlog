c43526a4c53b4846ea6e2b719096b5be86c5f252
Matt Bakken <mmbakken@gmail.com>
2021-01-13 23:39:35 -0700

    Better place for JWT checking is right in the AuthContext. Instead of only checking the JWT when we first hit a private route

--- src/components/Auth/PrivateRoute.js
+++ src/components/Auth/PrivateRoute.js
@@ -1,45 +1,13 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { Route, Redirect } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext'
 import { LoginRoute } from '../../constants/routes'
-import actions from '../../reducers/actions'
-import { APIv1, setAuthHeader } from '../../api'
 
 // A wrapper for <Route> that redirects to the login
 // screen if you're not yet authenticated.
 const PrivateRoute = ({ children, ...rest }) => {
-  const [auth, authDispatch] = useContext(AuthContext)
-
-  useEffect(() => {
-    // Check for a JWT, and save it to the AuthContext if set
-    const token = localStorage.getItem('token')
-
-    if (token != null) {
-      // Add the token to the API instance
-      setAuthHeader(token)
-
-      authDispatch({
-        type: actions.GET_USER__START,
-      })
-
-      // Go get the user information from the token
-      APIv1.get('/user')
-        .then((response) => {
-          // Then save the user info to auth state
-          authDispatch({
-            type: actions.GET_USER__SUCCESS,
-            user: response.data.user,
-          })
-        })
-        .catch((error) => {
-          console.error(error)
-          authDispatch({
-            type: actions.GET_USER__ERROR,
-          })
-        })
-    }
-  }, [auth.checkingJWT])
+  const auth = useContext(AuthContext)[0]
 
   // Don't load the /login page before we're had a chance to determine if the user is
   // already logged in!
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,12 +1,44 @@
-import React, { useReducer, createContext } from 'react'
+import React, { useReducer, createContext, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import initialState from '../reducers/initialState'
 import authReducer from '../reducers/authReducer'
+import actions from '../reducers/actions'
+import { APIv1, setAuthHeader } from '../api'
 
 export const AuthContext = createContext()
 
 export const AuthContextProvider = (props) => {
   const [auth, authDispatch] = useReducer(authReducer, initialState.auth)
+
+  useEffect(() => {
+    // Check for a JWT, and save it to the AuthContext if set
+    const token = localStorage.getItem('token')
+
+    if (token != null) {
+      // Add the token to the API instance
+      setAuthHeader(token)
+
+      authDispatch({
+        type: actions.GET_USER__START,
+      })
+
+      // Go get the user information from the token
+      APIv1.get('/user')
+        .then((response) => {
+          // Then save the user info to auth state
+          authDispatch({
+            type: actions.GET_USER__SUCCESS,
+            user: response.data.user,
+          })
+        })
+        .catch((error) => {
+          console.error(error)
+          authDispatch({
+            type: actions.GET_USER__ERROR,
+          })
+        })
+    }
+  }, [])
 
   return (
     <AuthContext.Provider value={[auth, authDispatch]}>
