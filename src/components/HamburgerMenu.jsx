eff56a1d7c93e263ef3a807ef47c9bc5dc1d6f1b
Matt Bakken <mmbakken@gmail.com>
2023-01-27 02:38:45 -0700

    HomePage shows off training calendar and all runs components. (#31)

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -13,7 +13,7 @@ import {
   AccountRoute,
 } from '../constants/routes'
 
-const HamburgerMenu = ({ isLoggedIn, logout }) => {
+const HamburgerMenu = ({ isLoggedIn, logout, isHomeRoute }) => {
   const history = useHistory()
   const location = useLocation()
   const [isOpen, setIsOpen] = useState(false)
@@ -37,7 +37,7 @@ const HamburgerMenu = ({ isLoggedIn, logout }) => {
   }, [location])
 
   return (
-    <div className='sm:hidden flex w-full'>
+    <div className='fixed bg-neutral-800 sm:hidden flex w-full'>
       <div className='flex relative items-center space-x-2'>
         <span onClick={onMenuClick} className='px-4'>
           {isOpen && (
@@ -55,20 +55,22 @@ const HamburgerMenu = ({ isLoggedIn, logout }) => {
           )}
         </span>
 
-        <span
-          className='cursor-pointer'
-          onClick={() => {
-            onLinkClick(HomeRoute)
-          }}
-        >
-          <header>
-            <h1 className='font-heading pb-2 text-4xl'>runlog</h1>
-          </header>
-        </span>
+        {!isHomeRoute || isOpen ? (
+          <span
+            className='cursor-pointer'
+            onClick={() => {
+              onLinkClick(HomeRoute)
+            }}
+          >
+            <header>
+              <h1 className='font-heading pb-2 text-4xl'>runlog</h1>
+            </header>
+          </span>
+        ) : null}
       </div>
 
       {isOpen && (
-        <ul className='z-50 flex flex-col absolute top-20 bottom-0 left-0 right-0 text-lg bg-neutral-100 border-t-2 border-eggplant-700'>
+        <ul className='z-50 flex flex-col fixed top-20 bottom-0 left-0 right-0 text-lg bg-neutral-800 border-t-2 border-eggplant-700'>
           <li
             className={'cursor-pointer border-b border-gray-300 px-4 py-4'}
             onClick={() => {
@@ -134,6 +136,7 @@ const HamburgerMenu = ({ isLoggedIn, logout }) => {
 
 HamburgerMenu.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired,
+  isHomeRoute: PropTypes.bool,
   logout: PropTypes.func.isRequired,
 }
 
