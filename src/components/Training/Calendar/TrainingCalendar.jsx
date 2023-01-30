a59470a4c1c405d673311a8ee93479e6a65237dd
Matthew Bakken <mmbakken@gmail.com>
2023-01-30 15:20:16 -0700

    Copy paste box no longer under selected weeks or dates

--- src/components/Training/Calendar/TrainingCalendar.jsx
+++ src/components/Training/Calendar/TrainingCalendar.jsx
@@ -570,7 +570,7 @@ const TrainingCalendar = ({ training, disableSelection, updatePlan }) => {
   return (
     <div className='TrainingCalendar flex flex-col z-0 px-4 pb-12 w-[1536px]'>
       {!disableSelection && showCopyPasteBox ? (
-        <div className='fixed bottom-4 left-1/2 -translate-x-1/2 z-20 flex justify-center space-x-4 bg-neutral-800 border rounded border-neutral-400 drop-shadow-lg px-4 py-2'>
+        <div className='fixed bottom-4 left-1/2 -translate-x-1/2 z-30 flex justify-center space-x-4 bg-neutral-800 border rounded border-neutral-400 drop-shadow-lg px-4 py-2'>
           <button
             className={copyBtnClasses}
             disabled={!allowCopy}
