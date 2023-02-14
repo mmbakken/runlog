c1507c2c217405f337cb71a8b310a1df23ee7f52
Matthew Bakken <mmbakken@gmail.com>
2023-02-14 00:13:45 -0700

    Removed gear subheader

--- src/components/User/Gear.jsx
+++ src/components/User/Gear.jsx
@@ -6,16 +6,9 @@ import ShoeList from './ShoeList'
 const Gear = ({ gear }) => {
   return (
     <div className='space-y-4'>
-      <header className='mx-4 mb-4'>
-        <h2 className='text-xl mb-2'>Gear</h2>
-        <p>
-          Add running gear, like shoes, so you can link them to your workouts
-          and track their mileage.
-        </p>
-      </header>
-
       <section className='mx-4'>
-        <h3 className='text-lg mb-2'>Shoes</h3>
+        <h2 className='text-lg mb-2'>Shoes</h2>
+        <p className='mb-4'>Add new shoes here and track their mileage.</p>
         <ShoeList shoes={gear?.shoes} />
       </section>
     </div>
