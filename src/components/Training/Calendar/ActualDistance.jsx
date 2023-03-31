92e74c7d425f4eaec46d2fc34657433f87413c59
Matthew Bakken <mmbakken@gmail.com>
2023-03-30 21:42:58 -0600

    Open the run(s) link in a new tab

--- src/components/Training/Calendar/ActualDistance.jsx
+++ src/components/Training/Calendar/ActualDistance.jsx
@@ -1,37 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useHistory } from 'react-router-dom'
 import { AllRunsRoute, ViewRunRoute } from '../../../constants/routes'
 import { formatActualMileage } from '../../../formatters/formatMileage'
 
 const ActualDistance = ({ isoDate, distance, runIds }) => {
-  const history = useHistory()
-
-  const handleClick = (isoDate, runIds) => {
-    if (runIds == null || runIds.length === 0) {
-      return
-    }
-
-    // Go to run's page if only one run today. Otherwise, go to AllRuns page with a filter param.
-    let url
-    if (runIds.length === 1) {
-      url = ViewRunRoute.replace(':runId', runIds[0])
-    } else {
-      url = `${AllRunsRoute}?date=${isoDate}`
-    }
-
-    history.push(url)
+  // Go to run's page if only one run today. Otherwise, go to AllRuns page with a filter param.
+  const url = new URL(window.location)
+  let route = ViewRunRoute.replace(':runId', runIds[0])
+  if (runIds.length > 1) {
+    route = `${AllRunsRoute}?date=${isoDate}`
   }
 
   if (distance > 0) {
     return (
-      <span
-        className='h-6 cursor-pointer hover:text-semibold hover:bg-eggplant-600 transition'
-        onClick={() => {
-          handleClick(isoDate, runIds)
-        }}
-      >
-        {formatActualMileage(distance)}
+      <span className='h-6 cursor-pointer hover:text-semibold hover:bg-eggplant-600 transition'>
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href={`${url.origin}${route}`}
+        >
+          {formatActualMileage(distance)}
+        </a>
       </span>
     )
   } else {
