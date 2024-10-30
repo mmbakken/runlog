dd119e27f3568ac81a5e4f88f35ebae39502e0ee
Matthew Bakken <mmbakken@gmail.com>
2024-10-29 20:09:51 -0600

    Upgrade ESLint to flat config, add ESLint plugins, and improve TS support.

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,122 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import tailwind from 'eslint-plugin-tailwindcss'
 import prettierConfig from 'eslint-config-prettier'
 
-import react from 'eslint-plugin-react'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import reactHooks from 'eslint-plugin-react-hooks'
-import tailwind from 'eslint-plugin-tailwindcss'
-import jsxA11y from 'eslint-plugin-jsx-a11y'
-import globals from 'globals'
-
 export default tseslint.config(
+  {
+    // Applies ignore rules to all configs below.
+    ignores: ['dist/*'],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  prettierConfig,
+
+  // TS linting.
   {
-    extends: [...tailwind.configs['flat/recommended']],
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    plugins: {
-      react,
-      'react-refresh': reactRefresh,
-      'react-hooks': reactHooks,
-      'jsx-a11y': jsxA11y,
-    },
+    files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parser: tseslint.parser,
       parserOptions: {
+        projectService: true,
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        tsconfigRootDir: import.meta.dirname,
         ecmaFeatures: {
           jsx: true,
         },
       },
-      globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      react: react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+      tailwindcss: tailwind,
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.flatConfigs.recommended.rules,
+      ...tailwind.configs['flat/recommended'].rules,
+
+      // Rule overrides
+      'react/prop-types': 0,
       'react-refresh/only-export-components': [
         'warn',
-        {
-          allowConstantExport: true,
-        },
+        { allowConstantExport: true },
       ],
 
-      'react/prop-types': [0],
+      'tailwindcss/no-custom-classname': [
+        'warn',
+        {
+          cssFiles: ['styles/root.css'],
+        },
+      ],
     },
-  }
+    settings: {
+      react: { version: '18.3' },
+    },
+  },
+
+  // JS linting. Similar plugins/rules as above.
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    ...tseslint.configs.disableTypeChecked,
+    languageOptions: {
+      ...tseslint.configs.disableTypeChecked.languageOptions,
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ...tseslint.configs.disableTypeChecked.languageOptions.parserOptions,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      ...tseslint.configs.disableTypeChecked.plugins,
+      react: react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+      tailwindcss: tailwind,
+    },
+    rules: {
+      ...tseslint.configs.disableTypeChecked.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.flatConfigs.recommended.rules,
+      ...tailwind.configs['flat/recommended'].rules,
+
+      // Rule overrides
+      'react/prop-types': 0,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+
+      'tailwindcss/no-custom-classname': [
+        'warn',
+        {
+          cssFiles: ['styles/root.css'],
+        },
+      ],
+    },
+    settings: {
+      ...tseslint.configs.disableTypeChecked.settings,
+      react: { version: '18.3' },
+    },
+  },
+
+  // Keep last.
+  prettierConfig
 )
